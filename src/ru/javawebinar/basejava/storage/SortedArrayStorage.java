2dd36b962551fab60f0224150cbf74faf7700e8e
Vladimir Tischenko <vladimirtischenko82@gmail.com>
2023-10-04 13:47:26 +0300

    Lesson06 Iterator, Comparator and Inner Classes

--- src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
@@ -3,8 +3,18 @@ package ru.javawebinar.basejava.storage;
 import ru.javawebinar.basejava.model.Resume;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class SortedArrayStorage extends AbstractArrayStorage {
+//    private static class ResumeComparator implements Comparator<Resume> {
+//        @Override
+//        public int compare(Resume o1, Resume o2) {
+//            return o1.getUuid().compareTo(o2.getUuid());
+//        }
+//    }
+
+    private static final Comparator<Resume> RESUME_COMPARATOR = Comparator.comparing(Resume::getUuid);
+
     @Override
     protected void insertResume(Resume r, int index) {
         int pos = -index - 1;
@@ -22,6 +32,6 @@ public class SortedArrayStorage extends AbstractArrayStorage {
     @Override
     protected int findIndex(String uuid) {
         Resume searchKey = new Resume(uuid);
-        return Arrays.binarySearch(storage, 0, size, searchKey);
+        return Arrays.binarySearch(storage, 0, size, searchKey, RESUME_COMPARATOR);
     }
 }
